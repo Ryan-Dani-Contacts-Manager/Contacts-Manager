7534177d93a09a8d49790885130eb90e02ede4fb
danielle lane <daniellelane1821@gmail.com>
2019-10-02 14:54:24 -0500

    trying to redo add contact

--- src/ContactTest.java
+++ src/ContactTest.java
@@ -1,18 +1,36 @@
 import java.util.HashMap;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ContactTest {
     public static void main(String[] args) {
-        Contact one = new Contact("dani","911");
+//        Contact one = new Contact("dani", "911");
 
-        HashMap<String, Contact> contacts = new HashMap<>();
-        addContact("dani","210", contacts);
-//        System.out.println(contacts.);
-        for(String contact : contacts.keySet()) {
-                System.out.println(contacts.get(contact).getPhoneNumber());
-            }
+//        HashMap<String, Contact> contacts = new HashMap<>();
+        List<Contact> contacts = new ArrayList<>();
+
+
+
+        Contact bob = new Contact("bob","298");
+
+            contacts.add(bob);
+            Path ContactListPath = Paths.get("src","ContactList.txt");
+//            try{
+//                Files.write(ContactListPath,contacts);
+//            }catch(Exception e){
+//                e.printStackTrace();
+//            }
+        System.out.println(bob.getName());
         }
 
-    public static void  addContact (String name, String number, HashMap contacts) {
-        contacts.put("contact", new Contact(name,number));
-    }
+
+
+
+
 }
